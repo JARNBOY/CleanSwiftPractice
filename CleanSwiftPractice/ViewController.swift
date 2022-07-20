9fd53680cbf47dd8bfb404946d21efb1f924e6d7
Papon <papon.s@lawyerplusgroup.com>
2022-07-20 11:18:24 +0700

    Initial Commit

---
+++ CleanSwiftPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftPractice
+//
+//  Created by Papon Supamongkonchai on 20/7/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
